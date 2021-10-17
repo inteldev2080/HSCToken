a935b2911bf6a49d7bc8460aee58375799bc776f
Teddy <inteldev2080@gmail.com>
2021-10-17 19:53:11 +0300

    feat: add .gitignore file

--- contracts/HSCToken.sol
+++ contracts/HSCToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
  
 //Safe Math Interface
  
@@ -29,9 +29,9 @@ contract SafeMath {
 //ERC Token Standard #20 Interface
  
 abstract contract ERC20Interface {
-    function totalSupply() public virtual returns (uint);
-    function balanceOf(address tokenOwner) public virtual returns (uint balance);
-    function allowance(address tokenOwner, address spender) public virtual returns (uint remaining);
+    function totalSupply() public virtual view returns (uint);
+    function balanceOf(address tokenOwner) public virtual view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public virtual view returns (uint remaining);
     function transfer(address to, uint tokens) public virtual returns (bool success);
     function approve(address spender, uint tokens) public virtual returns (bool success);
     function transferFrom(address from, address to, uint tokens) public virtual returns (bool success);
@@ -44,7 +44,7 @@ abstract contract ERC20Interface {
 //Contract function to receive approval and execute function in one call
  
 abstract contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public virtual;
+    function receiveApproval(address from, uint256 tokens, address token, bytes memory data) virtual public;
 }
  
 //Actual token contract
@@ -59,36 +59,36 @@ contract HSCToken is ERC20Interface, SafeMath {
     mapping(address => mapping(address => uint)) allowed;
  
     constructor() public {
-        symbol = "HSCT";
+        symbol = "HSC";
         name = "HSCToken";
         decimals = 6;
         _totalSupply = 1000000000000;
-        balances[msg.sender] = _totalSupply;
-        emit Transfer(address(0), msg.sender, _totalSupply);
+        balances[0xE7AFE8f22f66C4a7bF56ADe8285973b0A17D960a] = _totalSupply;
+        emit Transfer(address(0), 0xE7AFE8f22f66C4a7bF56ADe8285973b0A17D960a, _totalSupply);
     }
  
-    function totalSupply() override public returns (uint) {
+    function totalSupply() override public view returns (uint) {
         return _totalSupply  - balances[address(0)];
     }
  
-    function balanceOf(address tokenOwner) public override returns (uint balance) {
+    function balanceOf(address tokenOwner) override public view returns (uint balance) {
         return balances[tokenOwner];
     }
  
-    function transfer(address to, uint tokens) public override returns (bool success) {
+    function transfer(address to, uint tokens) override public returns (bool success) {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
  
-    function approve(address spender, uint tokens) public override returns (bool success) {
+    function approve(address spender, uint tokens) override public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
  
-    function transferFrom(address from, address to, uint tokens) public override returns (bool success) {
+    function transferFrom(address from, address to, uint tokens) override public returns (bool success) {
         balances[from] = safeSub(balances[from], tokens);
         allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
@@ -96,11 +96,11 @@ contract HSCToken is ERC20Interface, SafeMath {
         return true;
     }
  
-    function allowance(address tokenOwner, address spender) public override returns (uint remaining) {
+    function allowance(address tokenOwner, address spender) override public view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
  
-    function approveAndCall(address spender, uint tokens, bytes memory data) public virtual returns (bool success) {
+    function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, address(this), data);
